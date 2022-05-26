32f2de42ac8656568615a8821296297229ef6f6a
Skrrrrra <flsab45@gmail.com>
2022-05-26 16:53:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace LeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //путь
+
+            string inputpath = "D:\\SolutionsForSpaceApp\\2004\\input.txt";
+            string outputpath = "D:\\SolutionsForSpaceApp\\2004\\output.txt";
+
+            //создание файлов
+            FileStream fs = new FileStream(inputpath, FileMode.OpenOrCreate);
+            fs.Close();
+            FileStream fsout = new FileStream(outputpath, FileMode.OpenOrCreate);
+            fs.Close();
+
+            //cчитывание года из файла
+            int takeyear = Convert.ToInt32(File.ReadAllText(inputpath));
+
+            //сравнение и запись 1 или 0
+            if ((takeyear % 4 == 0) && (takeyear % 100 != 0) || (takeyear % 400 == 0))
+            {
+                File.WriteAllText(outputpath, 1.ToString());
+            }
+            else
+            {
+                File.WriteAllText(outputpath, 0.ToString());
+            }
+
+
+        }
+    }
+}
